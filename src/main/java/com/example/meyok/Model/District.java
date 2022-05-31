8e7f2e6eddaf2fea07f90b6bfe60f40e853d8cff
Elias Qayumi <63395888+eliasqayumi@users.noreply.github.com>
2022-05-31 12:59:03 +0300

    Update District.java

--- src/main/java/com/example/meyok/Model/District.java
+++ src/main/java/com/example/meyok/Model/District.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer"})
 public class District {
     @Id
     @Column(name = "district_id", nullable = false, length = 10)
